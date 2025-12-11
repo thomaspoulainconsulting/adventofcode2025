fb9e37856576e02cd00c0693d22b93d7c7ddeb03
Thomas Poulain <thomas.poulain.partner@decathlon.com>
2025-12-11 14:40:09 +0100

    chore: day 2 part 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Advent of Code - 2025 Edition
 
 - Day 1 - Secret Entrance 🌟🌟
-- Day 2 - Gift Shop 🌟
+- Day 2 - Gift Shop 🌟🌟
--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -31,7 +31,6 @@ class Day2 : Day(2, "Gift Shop") {
     }
 
     override fun solvePart2(input: List<String>): String {
-        return ""
         return input
             .first()
             .split(",")
@@ -43,21 +42,23 @@ class Day2 : Day(2, "Gift Shop") {
 
     /**
      * - starts with 0 -> not an ID
-     * - sequence of digits repeated twice -> a valid ID
+     * - sequence of digits repeated at least twice -> a valid ID
      */
     private fun findInvalidProductIdPart2(start: Long, end: Long): List<Long> {
         return buildList {
             for (i in start..end) {
                 val number = i.toString()
-                if (number[0] == '0') continue
+                if (number.startsWith('0')) continue
 
-                // créer les parts de 1 à length/2
-                // exemple 11, 1010, 123123
-                val parts = mutableListOf<String>()
-                for (partSize in 1..number.length / 2) {
-                    parts.addAll(number.chunked(partSize))
+                (number.length / 2 downTo 1).forEach { chunkSize ->
+                    number.chunked(chunkSize)
+                        .takeIf { chunked -> chunked.all { it == chunked.first() } }
+                        ?.let {
+                            if (!this.contains(i)) {
+                                add(i)
+                            }
+                        }
                 }
-                println(parts)
             }
         }
     }
